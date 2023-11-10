5a7069b484f1144f5bf0fa73e587091a51348ff6
Sorok <Sorok@DESKTOP-C9GGTD7>
2023-11-11 00:47:34 +0200

    Добавьте файлы проекта.

---
+++ lab Soroka.D 9/Program.cs
@@ -0,0 +1,13 @@
+﻿int[,] mas = new int[5, 5];
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i == j) mas[i, j] = 1;
+        if (j == 2 || i == 2) mas[i, j] = 1;
+        if (j + i == 4) mas[i, j] = 1;
+        if (i == 2 && j == 2) mas[i, j] = 0;
+        Console.Write(mas[i, j] + " ");
+    }
+    Console.WriteLine();
+}
